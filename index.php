b447e4223e314aa7ae53af5541c06a9b470fd332
M Rizky Syahputra <rizky@localhost.localdomain>
2018-08-04 00:09:20 +0700

    added index

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Submit POI - WisataOne</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+</head>
+<body>
+  <nav class="teal lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">WISATAOne</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">KLASEMEN</a></li>
+        <li><a href="#">WISATA</a></li>
+        <li><a href="#">MASUK/DAFTAR</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">KLASEMEN</a></li>
+        <li><a href="#">WISATA</a></li>
+        <li><a href="#">MASUK/DAFTAR</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+
+  <div class="container">
+      <h1>WISATAOne</h1>
+  </div>
+
+  <div class="container">
+
+    <div class="row">
+<!-- ### TOP 10 PLACES -->
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/parallax1.jpeg">
+            <!-- <span class="card-title">TOP 10 DESTINASI</span> -->
+          </div>
+          <div class="collection with-header">
+                  <span class="collection-header"><h4> Top 10 Destination</h4> </span>
+                  <a href="" class="collection-item"> 1. Sabang</a>
+                  <a href="" class="collection-item"> 2. Batoh</a>
+                  <a href="" class="collection-item"> 3. Lamdom</a>
+                  <a href="" class="collection-item"> 4. Peunayong</a>
+                  <a href="" class="collection-item"> 5. Sigli</a>
+                  <a href="" class="collection-item"> 6. Lhoknga</a>
+                  <a href="" class="collection-item"> 7. Ulee kareng</a>
+                  <a href="" class="collection-item"> 8. Lampuuk</a>
+                  <a href="" class="collection-item"> 9. Bandara SIM</a>
+                  <a href="" class="collection-item"> 10. Pelabuhan Malahayati</a>
+          </div>
+          <div class="card-action">
+            <a href="#">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+<!-- ### TOP 10 PENGGUNA -->
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/usercard.jpg">
+            <!-- <span class="card-title">TOP 10 DESTINASI</span> -->
+          </div>
+          <div class="collection with-header">
+                  <span class="collection-header"><h4> Top 10 Users</h4> </span>
+                  <a href="" class="collection-item"> 1. Kyjoo</a>
+                  <a href="" class="collection-item"> 2. IwanSyahp</a>
+                  <a href="" class="collection-item"> 3. Aziz</a>
+                  <a href="" class="collection-item"> 4. Rizky</a>
+                  <a href="" class="collection-item"> 5. Kucengaer</a>
+                  <a href="" class="collection-item"> 6. RizkySyahp</a>
+                  <a href="" class="collection-item"> 7. Ky700</a>
+                  <a href="" class="collection-item"> 8. Rizkysyahputra98</a>
+                  <a href="" class="collection-item"> 9. Andi</a>
+                  <a href="" class="collection-item"> 10. Budi</a>
+          </div>
+          <div class="card-action">
+            <a href="#">SELENGKAPNYA</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <span class="card-title">Stats</span>
+          <!-- <div class="card-action">
+            <a href="#">This is a link</a>
+          </div> -->
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+
+  <!-- <footer class="page-footer orange" style="position:fixed;bottom:0;left:0;width:100%;"> -->
+  <footer class="page-footer orange" style="bottom:0;left:0;width:100%;">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Kelompok 3</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
